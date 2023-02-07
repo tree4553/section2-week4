ff4ddddc1510acb4fafa169b9dee6fe353f055c3
tree4553 <tree4553@naver.com>
2023-02-08 01:02:20 +0900

    Bean을 이용한 Config 작성, 객체 생성

--- src/main/java/com/codestates/section2week4/DependencyConfig.java
+++ src/main/java/com/codestates/section2week4/DependencyConfig.java
@@ -4,20 +4,47 @@ import com.codestates.section2week4.coffee.CoffeeRepository;
 import com.codestates.section2week4.coffee.CoffeeService;
 import com.codestates.section2week4.member.MemberRepository;
 import com.codestates.section2week4.member.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// Java로 작성한 Config
+//public class DependencyConfig {
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    public MemberRepository memberRepository() {
+//        return new MemberRepository();
+//    }
+//
+//    public CoffeeService coffeeService() {
+//        return new CoffeeService(coffeeRepository());
+//    }
+//
+//    public CoffeeRepository coffeeRepository() {
+//        return new CoffeeRepository();
+//    }
+//}
+
+// Bean을 이용하여 작성한 Config
+@Configuration
 public class DependencyConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemberRepository();
     }
 
+    @Bean
     public CoffeeService coffeeService() {
         return new CoffeeService(coffeeRepository());
     }
 
+    @Bean
     public CoffeeRepository coffeeRepository() {
         return new CoffeeRepository();
     }
--- src/main/java/com/codestates/section2week4/SingletonTest.java
+++ src/main/java/com/codestates/section2week4/SingletonTest.java
@@ -2,6 +2,7 @@ package com.codestates.section2week4;
 
 import com.codestates.section2week4.member.MemberService;
 import com.codestates.section2week4.singleton.SingletonService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
     static DependencyConfig dependencyConfig = new DependencyConfig();
@@ -9,8 +10,14 @@ public class SingletonTest {
     static MemberService memberService1 = dependencyConfig.memberService();
     static MemberService memberService2 = dependencyConfig.memberService();
 
+    // Java에서 객체를 한개만 생성하여 공유하는 방식으로 객체 생성
     static SingletonService singletonService1 = SingletonService.getInstance();
     static SingletonService singletonService2 = SingletonService.getInstance();
+
+    // Spring의 Bean을 이용한 객체 생성
+    static AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(DependencyConfig.class);
+    static MemberService memberService3 = ac.getBean("memberService", MemberService.class);
+    static MemberService memberService4 = ac.getBean("memberService", MemberService.class);
 
     public static void main(String[] args) {
         // 같은 memberService 이지만 주소값이 다르다.
@@ -20,5 +27,9 @@ public class SingletonTest {
         // 같은 주소값이 출력된다.
         System.out.println("singletonService1 = " + singletonService1);
         System.out.println("singletonService2 = " + singletonService2);
+
+        // 같은 주소값이 출력된다.
+        System.out.println("memberService3 = " + memberService3);
+        System.out.println("memberService4 = " + memberService4);
     }
 }
